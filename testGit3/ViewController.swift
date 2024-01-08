baa26cd83f0569b933996ed99fc9c9caae0a12d5
ErmaIOS <tanikulovermekios@gmail.com>
2024-01-08 22:08:22 +0600

    secondCommit

--- testGit3/ViewController.swift
+++ testGit3/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private var age:Int = 40
+    
+    private  var name:String = "Erma"
+    
+    private var surName:String  = "Tani"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
